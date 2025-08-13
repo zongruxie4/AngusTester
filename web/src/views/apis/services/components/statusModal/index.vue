8631c864a6c7e5587a9645bd2fe07e075560bc43
HuQin6 <hq@xcan.cloud>
2025-08-13 09:25:05 +0800

    chore: shareModal locale config

--- web/src/views/apis/services/components/statusModal/index.vue
+++ web/src/views/apis/services/components/statusModal/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
 import { Hints, Modal, notification } from '@xcan-angus/vue-ui';
 import { apis, services } from 'src/api/tester';
 import SelectEnum from '@/components/SelectEnum/index.vue';
@@ -16,16 +17,18 @@ const props = withDefaults(defineProps<Props>(), {
   value: '',
   type: 'API'
 });
+
+const { t } = useI18n();
 const tipConfig = {
   // PROJECT: '修改项目时会同步修改项目下所有服务和接口状态。',
-  SERVICE: '修改服务时会同步修改服务下所有接口状态。',
+  SERVICE: t('service.statusModal.tips.serviceStatusChange'),
   API: ''
 };
 
 const publishTipConfig = {
   // PROJECT: '发布项目会发布项目下所有服务和接口，发布后的项目、服务、接口不允许修改保存。',
-  SERVICE: '发布项服务会发布服务下接口，发布后的服务、接口不允许修改保存。',
-  API: '发布接口后接口基本信息和参数信息不允许修改保存'
+  SERVICE: t('service.statusModal.tips.servicePublish'),
+  API: t('service.statusModal.tips.apiPublish')
 };
 
 const setStatusApiConfig = {
@@ -53,7 +56,7 @@ const submit = async () => {
   if (error) {
     return;
   }
-  notification.success('修改状态成功');
+  notification.success(t('service.statusModal.messages.updateStatusSuccess'));
   emits('confirm', { value: statusValue.value, message: statuName.value });
   close();
 };
@@ -70,7 +73,7 @@ watch(() => props.visible, newValue => {
 </script>
 <template>
   <Modal
-    title="修改状态"
+    :title="t('service.statusModal.title')"
     :visible="props.visible"
     :width="400"
     @cancel="close"
@@ -87,7 +90,7 @@ watch(() => props.visible, newValue => {
     </Hints>
     <div class="flex items-center">
       <span class="w-10">
-        状态
+        {{ t('service.statusModal.form.status') }}
       </span>
       <SelectEnum
         v-model:value="statusValue"
