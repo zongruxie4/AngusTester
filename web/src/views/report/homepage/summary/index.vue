0cdb9717f3266334c0262e15411548e0c11aa946
HuQin6 <hq@xcan.cloud>
2025-08-18 18:46:25 +0800

    chore：Report  summary locale config

--- web/src/views/report/homepage/summary/index.vue
+++ web/src/views/report/homepage/summary/index.vue
@@ -1,21 +1,24 @@
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
 import { Icon } from '@xcan-angus/vue-ui';
+
+const { t } = useI18n();
 </script>
 <template>
   <div class="flex content items-center flex-1">
     <img src="./images/logo.png" class="w-45 h-45" />
     <div class="ml-3.25 text-content flex-1">
-      <div>通过“报告”帮助团队可以全面总结研发活动的各个方面，包括进度、效能、质量、执行情况、结果分析等，这有助于提高透明度和沟通，为项目的成功交付提供有力支持。</div>
-      <div class="mt-2">在这里您和您的团队可以完成以下工作：</div>
+      <div>{{ t('reportHome.summary.description') }}</div>
+      <div class="mt-2">{{ t('reportHome.summary.workTitle') }}</div>
 
       <div class="mt-1">
         <Icon icon="icon-duihaolv" class="mr-2.5" />
-        <span>根据报告模版在指定时间生成报告，并通过事件通知到相关人。</span>
+        <span>{{ t('reportHome.summary.features.generateReport') }}</span>
       </div>
 
       <div class="mt-1">
         <Icon icon="icon-duihaolv" class="mr-2.5" />
-        <span>查看我添加的或者授权给我的报告及内容。</span>
+        <span>{{ t('reportHome.summary.features.viewReport') }}</span>
       </div>
     </div>
   </div>
