fff7cd666ffc4c7e57ac2635c81cbd662c8d08cf
HuQin6 <hq@xcan.cloud>
2025-08-18 11:08:50 +0800

    chore: project page locale config

--- web/src/views/project/project/list/process/index.vue
+++ web/src/views/project/project/list/process/index.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
 import { Icon } from '@xcan-angus/vue-ui';
 import TespPng from './Image/step.png';
+
+const { t } = useI18n();
 
 const listData = [
   {
     type: 'step',
-    text: '建立管理组织开发和测试活动的工作流程基本单元。为团队提供了一个集中化方式来计划、跟踪、报告和验收工作进展。',
-    title: '添加项目',
+    text: t('project.addProjectStep.text'),
+    title: t('project.addProjectStep.title'),
     icon: 'icon-chuangjianxiangmu'
   },
   {
@@ -15,8 +18,8 @@ const listData = [
   },
   {
     type: 'step',
-    text: '明确开发和测试活动的负责人、时间、目标、验收标准等。帮助团队有组织地开展活动，保证软件产品的质量和可靠性。',
-    title: '制定迭代和测试计划',
+    text: t('project.planIterationAndTest.text'),
+    title: t('project.planIterationAndTest.title'),
     icon: 'icon-jihua1'
   },
   {
@@ -25,8 +28,8 @@ const listData = [
   },
   {
     type: 'step',
-    text: '按照计划开展开发和测试活动。随时跟踪和管理项目中的工作任务、缺陷、需求、用户故事、测试等工作项。',
-    title: '开展开发和测试活动',
+    text: t('project.developAndTest.text'),
+    title: t('project.developAndTest.title'),
     icon: 'icon-ceshirenwu'
   },
   {
@@ -35,8 +38,8 @@ const listData = [
   },
   {
     type: 'step',
-    text: '总结研发活动的各个方面，包括项目进展、效能分析、数据资产、测试结果等。为项目的成功交付提供有力支持。',
-    title: '报告结果',
+    text: t('project.reportResults.text'),
+    title: t('project.reportResults.title'),
     icon: 'icon-dingdanmokuai'
   }
 ];
@@ -44,7 +47,7 @@ const listData = [
 </script>
 <template>
   <div>
-    <div class="text-3.5 font-semibold mb-2.5">项目流程</div>
+    <div class="text-3.5 font-semibold mb-2.5">{{ t('project.projectProcess') }}</div>
     <div class="flex items-center">
       <div v-for="(item, idx) in listData" :key="idx">
         <div v-if="item.type === 'step'" class="p-4 border rounded border-blue-border bg-blue-bg4">
