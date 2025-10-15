e016108fbcc0f993bb93f6c3f3f21d4576e85967
HuQin6 <hq@xcan.cloud>
2025-10-15 10:48:36 +0800

    chore： plugi main export

--- web/src/plugins/test/smtp/index.ts
+++ web/src/plugins/test/smtp/index.ts
@@ -1,5 +1,5 @@
 import Core from './core/index.vue';
-import DebugLog from './core/DebugLog/index.vue';
+import DebugLog from '@/plugins/test/core/DebugLog/index.vue';
 import DebugResult from './core/DebugResult/index.vue';
 
 import 'tailwindcss/components.css';
