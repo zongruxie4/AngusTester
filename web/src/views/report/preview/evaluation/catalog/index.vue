c952b3dfc1a01d7c094593fa72ac6ff6e7c46990
HuQin6 <hq@xcan.cloud>
2025-11-27 16:06:12 +0800

    chore： evaluation report

--- web/src/views/report/preview/evaluation/catalog/index.vue
+++ web/src/views/report/preview/evaluation/catalog/index.vue
@@ -16,15 +16,15 @@ const DashedLine = defineAsyncComponent(() => import('./dashedLine.vue'));
 
     <div class="space-y-3.5">
       <a href="#a1" class="flex items-center space-x-2.5">
-        <span class="h1">{{ t('reportPreview.serial.1') }}<em class="inline-block w-0.25 font-medium"></em></span>
+        <span class="h1">{{ t('reportPreview.serial.1') }}<em class="inline-block w-0.25 font-medium"></em>测评信息</span>
         <DashedLine />
       </a>
       <a href="#a1.1" class="flex items-center space-x-2.5">
-        <span>1.1<em class="inline-block w-4.25 font-medium"></em></span>
+        <span>1.1<em class="inline-block w-4.25 font-medium"></em>测评概览</span>
         <DashedLine />
       </a>
       <a href="#a1.2" class="flex items-center space-x-2.5">
-        <span>1.2<em class="inline-block w-4.25 font-medium"></em></span>
+        <span>1.2<em class="inline-block w-4.25 font-medium"></em>测评信息详情</span>
         <DashedLine />
       </a>
      
