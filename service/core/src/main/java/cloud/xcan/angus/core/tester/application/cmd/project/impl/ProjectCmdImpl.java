525a9d20dbc47013a78f4d2c24c38c8209745a67
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-12-10 15:50:13 +0800

    feat: update project deliverables upload logic to include project ID for improved association

--- service/core/src/main/java/cloud/xcan/angus/core/tester/application/cmd/project/impl/ProjectCmdImpl.java
+++ service/core/src/main/java/cloud/xcan/angus/core/tester/application/cmd/project/impl/ProjectCmdImpl.java
@@ -662,14 +662,15 @@ public class ProjectCmdImpl extends CommCmd<Project, Long> implements ProjectCmd
       protected IdKey<Long, Object> process() {
 
         if (dataType.equals(ProjectDataType.DELIVERABLES)) {
-          // Upload project deliverables
-          fileRemote.upload(
-              new MultipartFile[]{file}, null, UPLOAD_TESTER_FILES_BIZ_KEY,
-              null, true).orElseContentThrow();
-
           // Set project properties
           Project project = new Project();
           assembleProjectInfo(project);
+
+          // Upload project deliverables
+          fileRemote.upload(
+              new MultipartFile[]{file}, null, UPLOAD_TESTER_FILES_BIZ_KEY,
+              null, project.getId(), true).orElseContentThrow();
+
           // Create project
           return projectCmd.add0(project);
         }
